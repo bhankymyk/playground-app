4794d47251237784b2d5bab54ca68e4cecd7d738
bhankymyk <bankole123michael@gmail.com>
2024-01-27 17:23:34 +0100

    feat: Getting started

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js"
+
+
+createApp(App).mount('#app')
